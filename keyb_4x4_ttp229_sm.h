a020630395a9c76e1925b0a5f3e83d7d81772593
Бажен Левковець <bazhen.paseka@gmail.com>
2019-10-04 17:14:34 +0300

    перенесено в сабмодуль
    /
    
    
    .

--- keyb_4x4_ttp229_sm.h
+++ keyb_4x4_ttp229_sm.h
@@ -1,5 +1,6 @@
 #ifndef KEYBOARD_4X4_TTP229_SM_H_INCLUDED
 #define KEYBOARD_4X4_TTP229_SM_H_INCLUDED
+/***********************************/
 
 	#include <string.h>
 	#include <stdio.h>
@@ -7,12 +8,11 @@
 
 /***********************************/
 
-/***********************************/
+void TTP229_start(UART_HandleTypeDef * _d_uart);
+uint8_t TTP229_scan_keyboard();
+uint8_t Get_button_pressed_status (void);
+void Set_button_pressed_status (uint8_t);
 
-
-/***********************************/
-
-
-
+void ttp_delay(uint32_t t);
 
 #endif 	//	KEYBOARD_4X4_TTP229_SM_H_INCLUDED
